bd5983a0cda8a8ac02e4810b20b53ee2a8d47ac7
Dirham <dirhamsistem@gmail.com>
2017-03-10 06:11:03 +0800

    perbaikan

--- README.md
+++
@@ -1,4 +0,0 @@
-PHP Api Webhook LINE 
-Untuk tutorial lengkap ada di 
-https://medantechno.com/read/news/23/tutorial-membuat-bot-messaging-api-line
-
--- bot.php
+++ bot.php
@@ -7,8 +7,8 @@ copyright @ medantechno.com
 
 require_once('./line_class.php');
 
-$channelAccessToken = ''; //sesuaikan 
-$channelSecret = '';//sesuaikan
+$channelAccessToken = 'WvRp4gwmlXW7hrTzZh3oBssM+/7r8ERNj8G90XNb9QUQzfGlZY2sUp0R+LUyE5yNbrYtvJUgsqF/PxYjTDOTuWhkcGwLHpi+ojUz3u0ey7lNo6sJ9awqAyCg1St5HxH7JXw9BMYEuB4Z7OzmU6kTswdB04t89/1O/w1cDnyilFU='; //sesuaikan 
+$channelSecret = '1b8f124df990829860161bd736c204e6';//sesuaikan
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 
